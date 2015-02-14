eb5c6f38a8cedc630eba1214239d5fd39f141667
Mehul Patel <mehul8@gmail.com>
2015-02-14 00:45:54 -0800

    build first android release

---
+++ platforms/android/ant-build/IfYoureReadingThis-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/mehul/code/ifyourereadingthis/platforms/android/ant-build/IfYoureReadingThis-release-unsigned.apk : \
+/Users/mehul/code/ifyourereadingthis/platforms/android/ant-build/IfYoureReadingThis.ap_ \
+/Users/mehul/code/ifyourereadingthis/platforms/android/ant-build/classes.dex \
+/Users/mehul/code/ifyourereadingthis/platforms/android/CordovaLib/ant-build/classes.jar \
--- platforms/android/ant-gen/com/mehuljpatel/drake/BuildConfig.java
+++ platforms/android/ant-gen/com/mehuljpatel/drake/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.mehuljpatel.drake;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 }
